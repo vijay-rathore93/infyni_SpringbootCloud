6d3ebb3f91e283612fe2947685de8938e08f3f16
vijay-rathore93 <vijaylearning93@gmail.com>
2022-07-08 07:25:43 +0530

    added code

--- Order-Service/src/main/java/org/application/OrderServiceApplication.java
+++ Order-Service/src/main/java/org/application/OrderServiceApplication.java
@@ -3,9 +3,11 @@ package org.application;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
